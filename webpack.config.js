23b3f20b874c380aa68d6c2f74110dd7f4d5deb3
Angela Huang <angelahuang89@gmail.com>
2018-03-12 16:35:59 -0700

    (feat) add mode, entry point, publicPath, and plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,29 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
 const SRC_DIR = path.join(__dirname, '/client/src');
 const DIST_DIR = path.join(__dirname, '/client/dist');
 
 module.exports = {
-  entry: `${SRC_DIR}/index.jsx`,
+  mode: 'development',
+  entry: ['webpack-hot-middleware/client', `${SRC_DIR}/index.jsx`],
   output: {
     filename: 'bundle.js',
     path: DIST_DIR,
+    publicPath: '/client/dist',
   },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Development',
+    }),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+  ],
   module: {
     rules: [
       {
