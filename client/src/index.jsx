2749d37edb05d9ce083c29e0c016b57878b277ba
Angela Huang <angelahuang89@gmail.com>
2018-03-12 14:36:01 -0700

    (init) add index.jsx

---
+++ client/src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => (
+  <div>
+    let's hang out in san francisco!
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById('app'));
