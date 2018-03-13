9672151e0bd59ead029cf8e01453e13b7f0d8c00
Angela Huang <angelahuang89@gmail.com>
2018-03-12 17:10:03 -0700

    (init) add server index.js

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+
+const app = express();
+const config = require('../webpack.config.js');
+
+const compiler = webpack(config);
+
+app.use(webpackDevMiddleware(compiler, {
+  publicPath: config.output.publicPath,
+}));
+
+const PORT = 3000;
+
+app.listen(PORT);
