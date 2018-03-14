34f658f6c37a22331a90edf705d203f21df5d0a5
Angela Huang <angelahuang89@gmail.com>
2018-03-13 19:34:26 -0700

    (init) create server file separate from index file
    refactor from require to import statements

---
+++ server/server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import webpack from 'webpack';
+import webpackDevMiddleware from 'webpack-dev-middleware';
+import webpackHotMiddleware from 'webpack-hot-middleware';
+import config from '../webpack.config.js';
+
+const app = express();
+const compiler = webpack(config);
+
+app.use(webpackDevMiddleware(compiler, {
+  publicPath: config.output.publicPath,
+}));
+
+app.use(webpackHotMiddleware(compiler));
+
+export default app;
