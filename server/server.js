a73957204b9eef76cc9cfaa8b5011cb8e64bc964
Angela Huang <angelahuang89@gmail.com>
2018-03-14 17:07:58 -0700

    (refactor) update server to check for development environment

--- server/server.js
+++ server/server.js
@@ -1,16 +1,8 @@
 import express from 'express';
-import webpack from 'webpack';
-import webpackDevMiddleware from 'webpack-dev-middleware';
-import webpackHotMiddleware from 'webpack-hot-middleware';
-import config from '../webpack.dev.config.js';
+import checkEnvironment from '../helpers/envCheck';
 
 const app = express();
-const compiler = webpack(config);
 
-app.use(webpackDevMiddleware(compiler, {
-  publicPath: config.output.publicPath,
-}));
-
-app.use(webpackHotMiddleware(compiler));
+checkEnvironment(app);
 
 export default app;
