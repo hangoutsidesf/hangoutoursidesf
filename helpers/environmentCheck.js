b40d3f51ca84fcaead63adddddc0ddb3bded9a91
Angela Huang <angelahuang89@gmail.com>
2018-03-14 17:09:22 -0700

    (feat) create function to check for dev environment and use webpack dev and hot middleware

---
+++ helpers/environmentCheck.js
@@ -0,0 +1,19 @@
+import {} from 'dotenv/config';
+
+const checkEnvironment = (app) => {
+  if (process.env.NODE_ENV === 'development') {
+    const webpack = require('webpack');
+    const webpackDevMiddleware = require('webpack-dev-middleware');
+    const webpackHotMiddleware = require('webpack-hot-middleware');
+    const config = require('../webpack.dev.config.js');
+    const compiler = webpack(config);
+
+    app.use(webpackDevMiddleware(compiler, {
+      publicPath: config.output.publicPath,
+    }));
+
+    app.use(webpackHotMiddleware(compiler));
+  }
+};
+
+export default checkEnvironment;
