0534b3f1eb9454d56e72c6455a0334951621d5a6
andino <andy.andino93@gmail.com>
2018-03-15 09:56:04 -0700

    (feat) add morgan middleware on env=development

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 import express from 'express';
 
 import useWebpackMiddleware from './environment/useWebpackMiddleware';
+import useMorganMiddleware from './environment/useMorganMiddleware';
+import router from './routers/test';
 
 const app = express();
 
 useWebpackMiddleware(app);
+useMorganMiddleware(app);
+app.use('/test', router);
 
 export default app;
