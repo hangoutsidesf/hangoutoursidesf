77adc2f4651543926db0a4cf86d6b06e72bddf9f
Mike Hernandez <mkhern@gmail.com>
2018-03-18 10:19:10 -0700

    Fetches parklet data temporarily from SFData endpoint.

--- client/src/mapView.jsx
+++ client/src/mapView.jsx
@@ -2,20 +2,40 @@ import React, { Component } from 'react';
 import { Map, TileLayer } from 'react-leaflet';
 import { tileSet, SFGeo, zoomLevel, mapAttribution } from '../mapconfig';
 
+const PARKLETS_ENDPOINT = 'https://data.sfgov.org/resource/6a7x-cttf.json';
+
 export default class MapView extends Component {
   constructor() {
     super();
     this.state = {
       center: [SFGeo[0], SFGeo[1]],
       zoom: zoomLevel,
+      parklets: [],
+      error: '',
     };
   }
 
+  componentDidMount() {
+    fetch(PARKLETS_ENDPOINT)
+      .then(response => response.json())
+      .then(json => json.map(parklet => ({
+        title: parklet.applicant,
+        coordinates: parklet.location.coordinates,
+      })))
+      .then(data => this.setState({ parklets: data }))
+      .catch(err => this.setState({ error: err }));
+  }
+
   render() {
-    const { center, zoom } = this.state;
+    const {
+      center, zoom, parklets, error,
+    } = this.state;
     return (
       <Map center={center} zoom={zoom} >
         <TileLayer attribution={mapAttribution} url={tileSet} />
+        {error &&
+          <h3>Issue loading parklets: {error}</h3>
+        }
       </Map>
     );
   }
