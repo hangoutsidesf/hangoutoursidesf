9d64d61050210c8799105dde0ce6082611370455
Mike Hernandez <mkhern@gmail.com>
2018-03-19 12:15:56 -0700

    Adds tests related to initial fetch and state.

--- spec/client/mapView.spec.jsx
+++ spec/client/mapView.spec.jsx
@@ -3,6 +3,8 @@ import { shallow } from 'enzyme';
 import fetchMock from 'fetch-mock';
 import dummyData from '../../client/parklets.json';
 import MapView from '../../client/src/mapView';
+import MarkerCollection from '../../client/src/markerCollection';
+import Marker from '../../client/src/marker';
 
 const url = 'https://data.sfgov.org/resource/6a7x-cttf.json';
 
@@ -32,24 +34,42 @@ describe('Map loads on screen properly', () => {
 });
 
 describe('Marker data loads after fetch', () => {
-  it('contains all parklets', () => {
 
+  fetchMock.restore();
+  fetchMock.getOnce(url, {
+    status: 200,
+    body: dummyData,
+  });
+
+  const wrapper = shallow(<MapView />);
+  it('contains all parklets', () => {
+    const { parklets } = wrapper.state();
+    expect(parklets).toHaveLength(32);
   });
 
   it('corrects the coordinates from lon-lat to lat-lon', () => {
-
+    const firstParkletCoordinates = wrapper.state().parklets[0].position;
+    expect(firstParkletCoordinates).toEqual([37.760648871591, -122.504335015934]);
   });
 
-  it('renders the error message when the fetch is unsuccessful', () => {
+  xit('renders the error message when the fetch is unsuccessful', () => {
 
   });
 
   it('renders an icon for a given marker', () => {
-
+    const markerWrapper = shallow(<Marker detail="" position={[]} displayModal={() => {}} />);
+    expect(markerWrapper.props().icon).toBeDefined();
   });
 });
 
 describe('User clicks on a marker', () => {
+
+  fetchMock.restore();
+  fetchMock.getOnce(url, {
+    status: 200,
+    body: dummyData,
+  });
+
   it('hides the leaflet popup', () => {
 
   });
--- spec/setup.js
+++ spec/setup.js
@@ -1,6 +1,5 @@
 /* eslint-disable */
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import nodeFetch from 'node-fetch';
 
 configure({ adapter: new Adapter() });
