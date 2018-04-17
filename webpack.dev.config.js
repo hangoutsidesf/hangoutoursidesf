d75e734995e734341e01feb95b2685f277dbdf61
BrendanKelly94 <brendan.kelly94@gmail.com>
2018-04-16 18:57:21 -0700

    Service worker registered without errors

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,8 +1,10 @@
 import path from 'path';
 import webpack from 'webpack'; // eslint-disable-line import/no-extraneous-dependencies
+import SWPrecacheWebpackPlugin from 'sw-precache-webpack-plugin';
 
 const SRC_DIR = path.join(__dirname, '/client/src');
 const DIST_DIR = path.join(__dirname, '/client/dist');
+const PUBLIC_PATH = 'http://localhost:8080';
 
 module.exports = {
   mode: 'development',
@@ -11,13 +13,29 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: DIST_DIR,
-    publicPath: '/client/dist',
+    publicPath: PUBLIC_PATH,
   },
   plugins: [
     new webpack.NamedModulesPlugin(),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.NoEmitOnErrorsPlugin(),
+    new SWPrecacheWebpackPlugin({
+      cacheId: 'hangoutsidesf',
+      filename: 'hangoutsidesf-service-worker.js',
+      staticFileGlobs: [
+        'client/*.css',
+        'client/*.html',
+        'client/mapconfig.js'
+      ],
+      minify: true,
+      navigateFallback: PUBLIC_PATH + '/',
+      staticFileGlobsIgnorePatterns: [/\.map$/, /manifest\.json$/],
+      runtimeCaching: [{
+        urlPattern: "(.*)",
+        handler: "cacheFirst"
+      }]
+    })
   ],
   module: {
     rules: [
