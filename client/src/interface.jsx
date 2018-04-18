da427c51ad68e9038c21ce40601ea3a375e8f98f
Justin <jvcruz@ucdavis.edu>
2018-04-17 21:25:41 -0700

    (refactor) resolve linting error and change apostrophe to escaped counterpart

--- client/src/interface.jsx
+++ client/src/interface.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 
 export default () => (
-	<div className="interface">
-		<button className="btn btn-dark address">Enter address</button>
-		<button className="btn btn-dark open">Open now</button>
-		<button className="btn btn-dark food"><i className="fas fa-coffee"></i></button>
-		<button className="btn btn-dark-outline wifi"><i className="fas fa-wifi"></i></button>
+  <div className="interface">
+    <button className="btn btn-dark address">Enter address</button>
+    <button className="btn btn-dark open">Open now</button>
+    <button className="btn btn-dark food"><i className="fas fa-coffee" /></button>
+    <button className="btn btn-dark-outline wifi"><i className="fas fa-wifi" /></button>
     <div className="content">
-      <h1>What's a parklet?</h1>
-      <p>Ever see what looks like a parking spot in front of a coffee shop replaced by a picnic bench and garden? That's an example of a parklet.</p>
+      <h1>What&apos;s a parklet?</h1>
+      <p>
+        Ever see what looks like a parking spot in front of a coffee shop replaced
+        by a picnic bench and garden? That&apos;s an example of a parklet.
+      </p>
     </div>
     <h2 className="bottom-question">Idea or Issue?</h2>
-	</div>
+  </div>
 );
