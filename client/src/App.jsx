4a950dad00fda43f68048d7c2df8165d0b84a223
Justin <jvcruz@ucdavis.edu>
2018-05-02 00:37:15 -0700

    (feat) change App to be stateful and create a function that handles filters

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import MapView from './mapView';
 import Interface from './Interface';
 
-const App = () => (
-  <div id="home">
-    <MapView />
-    <Interface />
-  </div>
-);
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      activeParklets: [],
+      hiddenParklets: [],
+    };
+  }
+
+  componentDidMount() {
+    this.handleFilters();
+  }
+  handleFilters() {
+    const open = document.querySelector('.open');
+    const food = document.querySelector('.food');
+    const wifi = document.querySelector('.wifi');
+    const { activeParklets, hiddenParklets } = this.state;
+    const parklets = [...activeParklets, ...hiddenParklets];
+    const show = [];
+    const hide = [];
+
+    console.log(open, food, wifi);
+
+    parklets.forEach(parklet => {
+    
+    });
+  }
+
+  render() {
+    return (
+      <div id="home">
+        <MapView />
+        <Interface />
+      </div>
+    );
+  }
+}
 
 export default App;
