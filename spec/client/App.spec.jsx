6da1d7422c106a2e9e36a0aeface31a6d224e0c5
Justin <jvcruz@ucdavis.edu>
2018-05-21 22:17:00 -0700

    (refactor) remove imported unused modules on spec files

--- spec/client/App.spec.jsx
+++ spec/client/App.spec.jsx
@@ -1,12 +1,5 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { shallow } from 'enzyme';
-import fetchMock from 'fetch-mock';
-
-import dummyData from '../../client/parklets.json';
-import App from '../../client/src/App';
-
-const url = 'https://data.sfgov.org/resource/6a7x-cttf.json';
 
 describe('App component', () => {
   xtest('App should match snapshot', () => {
