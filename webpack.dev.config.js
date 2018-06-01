dbc8ca8c186ae20ac610bd7088938d73f85efa49
BrendanKelly94 <brendan.kelly94@gmail.com>
2018-06-01 09:40:07 -0700

    adjusted runtime image cache size and implemented cache busting for bundle

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,7 +1,8 @@
 import path from 'path';
 import webpack from 'webpack'; // eslint-disable-line import/no-extraneous-dependencies
-import SWPrecacheWebpackPlugin from 'sw-precache-webpack-plugin';
 import WorkboxPlugin from 'workbox-webpack-plugin';
+import HtmlWebpackPlugin from 'html-webpack-plugin'; //eslint-disable-line
+import CleanWebpackPlugin from 'clean-webpack-plugin';
 
 const SRC_DIR = path.join(__dirname, '/client/src');
 const DIST_DIR = path.join(__dirname, '/client/dist');
@@ -12,8 +13,8 @@ module.exports = {
   resolve: { extensions: ['.js', '.jsx', '.json'] },
   entry: ['webpack-hot-middleware/client', `${SRC_DIR}/index.jsx`],
   output: {
-    filename: 'bundle.js',
-    path: DIST_DIR
+    filename: 'bundle.[hash].js',
+    path: DIST_DIR,
   },
   plugins: [
     new webpack.NamedModulesPlugin(),
@@ -27,24 +28,29 @@ module.exports = {
       // Define runtime caching rules.
       runtimeCaching: [
         {
-          // Match any request ends with .png, .jpg, .jpeg or .svg.
+          // Match any request ends with .jpg
           urlPattern: /http.*jpg/,
           // Apply a cache-first strategy.
           handler: 'cacheFirst',
           options: {
             cacheName: 'image-cache',
             expiration: {
-              maxEntries: 20
+              maxEntries: 40,
             },
             cacheableResponse: { statuses: [0, 200] },
           },
         },
         {
+          // Cache parklet json
           urlPattern: /http.*json/,
-          handler: 'cacheFirst'
-        }
+          handler: 'cacheFirst',
+        },
       ],
-    })
+    }), new HtmlWebpackPlugin({
+      hash: true,
+      template: path.join(__dirname, 'client', 'index.html'),
+    }),
+    new CleanWebpackPlugin(['client/dist']),
   ],
   module: {
     rules: [
